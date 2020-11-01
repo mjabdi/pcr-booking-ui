e73571a4f8b4f905ed8fa2b645f70a9eed6915c9
unknown <m.jafarabdi@gmail.com>
2020-11-01 16:38:53 +0330

    stage2

--- src/services/TimeService.js
+++ src/services/TimeService.js
@@ -3,9 +3,9 @@ import API from './api';
 
 export default class TimeService{
 
-     static getFirstAvailableDay =  () =>
+     static getFirstAvailableDate =  () =>
     {
-        return API.get('/api/time/getfirstavaiableday');
+        return API.get('/api/time/getfirstavaiabledate');
     }
 
     static dayIsBooked =  (date) =>
@@ -19,7 +19,7 @@ export default class TimeService{
        });
     }
 
-    static getFullyBookedDays = () =>
+    static getFullyBookedDates = () =>
     {
        /// check if this date is booked or not
        return new Promise( (resolve, reject) =>
@@ -35,43 +35,7 @@ export default class TimeService{
 
     static getTimeSlots = (date) =>
     {
-        return new Promise( (resolve, reject) =>
-        {
-            const SampleArray = [
-                new TimeSlot('09:00 AM', true),
-                new TimeSlot('09:15 AM', true),
-                new TimeSlot('09:30 AM', true),
-                new TimeSlot('10:00 AM', true),
-                new TimeSlot('10:15 AM', true),
-                new TimeSlot('10:30 AM', true),
-                new TimeSlot('11:00 AM', true),
-                new TimeSlot('11:15 AM', true),
-                new TimeSlot('11:30 AM', true),
-                new TimeSlot('12:00 AM', true),
-                new TimeSlot('12:15 PM', true),
-                new TimeSlot('12:30 PM', false),
-                new TimeSlot('01:00 PM', true),
-                new TimeSlot('01:15 PM', true),
-                new TimeSlot('01:30 PM', true),
-                new TimeSlot('02:00 PM', true),
-                new TimeSlot('02:15 PM', true),
-                new TimeSlot('02:30 PM', true),
-                new TimeSlot('03:00 PM', false),
-                new TimeSlot('03:15 PM', true),
-                new TimeSlot('03:30 PM', true),
-                new TimeSlot('04:00 PM', true),
-                new TimeSlot('04:15 PM', true),
-                new TimeSlot('04:30 PM', false),
-                new TimeSlot('05:00 PM', true),
-                new TimeSlot('05:15 PM', true),
-                new TimeSlot('05:30 PM', true),
-                new TimeSlot('05:45 PM', true),
-            ];
-
-            setTimeout(() => {
-                resolve(SampleArray);
-            }, 1000);
-        });
+        return API.get(`/api/time/gettimeslots?date=${date}`);
     }
 
 
